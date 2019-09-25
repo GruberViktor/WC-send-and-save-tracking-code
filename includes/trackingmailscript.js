e5f8d43bc66c23d00b5d1919d3907056a18b60b8
Viktor Gruber <viktor@fermentationculture.eu>
2019-09-25 20:43:05 +0200

    Edited Javascript file, removed old php file

--- includes/trackingmailscript.js
+++ includes/trackingmailscript.js
@@ -7,13 +7,11 @@ jQuery(document).ready(function($) {
 	var data = {
 	    action: 'trackingmail',
 		code: $('#code').val(),
-		to: $('#customeremail').val(),
-		fname: $('#fname').val(),
 		order_id: $('#order_id').val()
 	}
 
 	jQuery.post(window.location.origin + "/wp-admin/admin-ajax.php", data, function(response) {
-	    $("#trackingmailform").after( "<p>Email sent</p>" );
+	    $("#trackingmailform").after( response );
 		});
 
 
